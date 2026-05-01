18c961c36c8ab3e7dffa46e4cbbbde9c50655404
Ethan <60394181+firecv@users.noreply.github.com>
2026-05-02 00:23:49 +0200

    Version 2 of site - WIP
    Restyled landing screen & colors

--- js/script.js
+++ js/script.js
@@ -3,16 +3,6 @@ window.onload = function() {
   document.body.scrollTop = 0;
 };
 
-window.onscroll = function() {shrinkHeader()};
-
-/*function shrinkHeader() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("header").style.padding = "5px";
-  } else {
-    document.getElementById("header").style.padding = "20px";
-  }
-}*/
-
 var splashScreen = document.getElementById("splash");
 var splashButton = document.getElementById('splash-button');
 splashButton.addEventListener('click', enableScroll);
@@ -22,22 +12,13 @@ function enableScroll() {
   document.body.classList.remove("no-scroll"); 
 }
 
-const acc = document.querySelectorAll('.accordion');
-const panels = document.querySelectorAll('.panel');
-
-acc.forEach((btn, i) => {
-  btn.addEventListener('click', () => {
-    panels.forEach((panel, j) => {
-      if (i === j && !panel.classList.contains('open')) {
-        panel.classList.add('open');
-        acc[j].classList.add('active');
-      } else if (i != j && panel.classList.contains('open')) {
-        panel.classList.remove('open');
-        acc[j].classList.remove('active');
-      }
-    });
-    document.documentElement.style.scrollBehavior = "auto";
-    //window.scrollTo(0, document.body.scrollHeight);
-    document.documentElement.style.scrollBehavior = "smooth";
-  });
+var bioSection = document.getElementById("bio-section");
+window.addEventListener("scroll", function(){
+  if(window.scrollY < 100){
+    bioSection.classList.add("bio-box");
+    bioSection.classList.remove("bio-box-scroll");
+  } else {
+    bioSection.classList.remove("bio-box");
+    bioSection.classList.add("bio-box-scroll");
+  }
 });
